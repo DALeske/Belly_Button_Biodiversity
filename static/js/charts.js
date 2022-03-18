6c2aa9b65cbcff1f846cf37188bddb92d67e9d56
David Leske <leske.david@gmail.com>
2022-03-17 19:56:11 -0500

    Deliv 1 - 2 and half of 3

--- static/js/charts.js
+++ static/js/charts.js
@@ -59,45 +59,112 @@ function buildCharts(sample) {
   d3.json("/static/js/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
-    console.log(samples);
+    var metadata = data.metadata;
+    
+    //console.log(samples);
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-    console.log(resultArray);
+    //console.log(resultArray);
+    //Gauge1 - Create a variable that filters the metadata array for the object with the desired sample number.
+    var  metaArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    //console.log(metaArray);
+
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
-    console.log(result);
+    //console.log(result);
+
+    // Gauge 2. Create a variable that holds the first sample in the metadata array.
+    var metaResult = metaArray[0];
+    //console.log(metaResult);
+
+
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var bb_otu_ids = result.otu_ids;
     var bb_otu_labels = result.otu_labels;
     var bb_sample_values = result.sample_values;
 
-    console.log(bb_otu_ids);
-    console.log(bb_otu_labels);
-    console.log(bb_sample_values);
+    //console.log(bb_otu_ids);
+    //console.log(bb_otu_labels);
+    //console.log(bb_sample_values);
+
+    // Guage 3. Create a variable that holds the washing frequency.
+    var washFreq = metaResult.wfreq;
+    //console.log(washFreq);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-    var yticks = bb_otu_ids.map(sampleObj => sampleObj.id == sample).slice(0,11).reverse()
-    console.log(yticks);
+    var yticks = bb_otu_ids.map(row=> "OTU" + row).slice(0,10).reverse();
+   //console.log(yticks);
+    var xvals = bb_sample_values.slice(0,10).reverse();
+   //console.log(xvals);
+    var labels = bb_otu_labels.slice(0,10).reverse();
+    //console.log(labels);
 
     // 8. Create the trace for the bar chart. 
     var trace = {
-      x: bb_sample_values,
-      y: bb_otu_ids_labels,
-      type: "bar"
+      x: xvals,
+      y: yticks,      
+      type: "bar",
+      orientation: "h",
+      text: labels,
+      hovertemplate: labels
     };
 
     var barData = [trace];
 
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-      title: "XXXX",
-      xaxis: {title: "sample values"},
-      yaxis: {title: "sample labels"}
+      title: "Top 10 Bacterial Cultures Found",
+      xaxis: {title: "Number of Bacteria"},
+      yaxis: {title: "",
+              text: labels}
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
+
+   // BUBBLE CHART   
+    // 1. Create the trace for the bubble chart.
+        
+    
+    var bubbleData = [{
+      x: bb_otu_ids,
+      y: bb_sample_values,
+      mode:"markers",
+      marker: {color: bb_otu_ids, 
+        size: bb_sample_values},
+      text: bb_otu_labels,
+      hovertemplate: bb_otu_labels
+      
+    }   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures per Sample",
+      xaxis: {title:"OTU ID"},
+      showlegend: false     
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
+
+    // GUAGE CHART
+        
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+     
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     
+    };
+
+
+
+
   });
 }
